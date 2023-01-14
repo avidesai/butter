7f339870f03b795caf4ce74f3825824199f06082
Avi Desai <avidesai2@gmail.com>
2023-01-13 22:11:36 -0800

    Made small changes

--- my-betting-app/src/App.js
+++ my-betting-app/src/App.js
@@ -1,10 +1,16 @@
 import './App.css'
 import HomePage from './components/HomePage';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>butter</title>
+        <meta name="description" content="Bet on the world." />
+        <link rel="icon" href="butter.png" />
+      </Helmet>
       <HomePage />
     </div>
   );
