61badff7158dd4904ec7c3589d82e56a4a8b79e4
Avi Desai <avidesai2@gmail.com>
2023-01-15 23:27:48 -0800

    done tweaking the modal for now

--- my-betting-app/src/components/BetModal.js
+++ my-betting-app/src/components/BetModal.js
@@ -5,17 +5,14 @@ const BetModal = ({ isOpen, closeModal, handleBetAmount, data }) => {
   const { title, price } = data;
   return (
     <div className={`modal-overlay ${isOpen ? 'is-open' : ''}`}>
-        <div className={`modal-content ${isOpen ? 'is-open' : ''}`}>
+      <div className={`modal-content ${isOpen ? 'is-open' : ''}`}>
             <button onClick={closeModal} className="modal-close-button">X</button>
             <h3>{title}</h3>
             <h4>{price}</h4>
-            <form onSubmit={handleBetAmount}>
-                <label>
-                    Enter bet amount:
-                    <input type="number" name="betAmount" min="1" required />
-                </label>
-                <button type="submit">Submit</button>
-            </form>
+            <p>Enter bet amount:</p>
+            <div className="button input"></div>
+            <input type="number" name="betAmount" min="1" required />
+            <button type="submit">Submit</button>
         </div>
     </div>
   );
