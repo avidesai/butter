bee9eb3c697ca4405d82995e9ab1132d99d9dbc6
Avi Desai <avidesai2@gmail.com>
2023-01-16 21:35:36 -0800

    Fixed things up a little more

--- my-betting-app/src/components/Navbar.js
+++ my-betting-app/src/components/Navbar.js
@@ -5,11 +5,20 @@ function Navbar({setIsBetsPage, setIsWalletPage, setIsUserPage, setIsHomePage}) 
 
   const handleBets = () => {
     setIsBetsPage(true);
+    setIsHomePage(false);
+    setIsWalletPage(false);
+    setIsUserPage(false);
   }
   const handleWallet = () => {
+    setIsBetsPage(false);
+    setIsHomePage(false);
     setIsWalletPage(true);
+    setIsUserPage(false);
   }
   const handleUser = () => {
+    setIsBetsPage(false);
+    setIsHomePage(false);
+    setIsWalletPage(false);
     setIsUserPage(true);
   }
   const handleHomeClick = () => {
@@ -26,9 +35,9 @@ function Navbar({setIsBetsPage, setIsWalletPage, setIsUserPage, setIsHomePage}) 
         </div>
         <div className="navbar-right">
             <div className="navbar-buttons">
-              <button onClick={handleBets}>&#128640;</button>
+              <button onClick={handleBets}>&#128100;</button>
               <button onClick={handleWallet}>&#128176;</button>
-              <button onClick={handleUser}>&#128100;</button>
+              <button onClick={handleUser}>&#128640;</button>
             </div>
         </div>
     </nav>
